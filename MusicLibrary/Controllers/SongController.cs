4adc793977d1e21ffecf8a96ce8be369ad0a99f1
M-Jakowczyk <mateusz.jakowczyk@o2.pl>
2024-05-20 17:15:29 +0200

    add SongController.cs

---
+++ MusicLibrary/Controllers/SongController.cs
@@ -0,0 +1,23 @@
+﻿using MusicLibrary.Db;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace MusicLibrary.Controllers
+{
+    internal class SongController
+    {
+        private static SongInventory _songInventory = new SongInventory();
+
+        public void ShowAllSongs()
+        {
+            foreach (var song in _songInventory.GetAllSongs())
+            {
+                Console.WriteLine($"[{song.Id}] {song.Title} - {song.Author} ({song.Year}) /{song.Album}/");
+            }
+        }
+    }
+}
